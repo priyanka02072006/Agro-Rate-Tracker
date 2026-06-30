9ebc6c3ee82d8990f8cccbdb239b8b73c5b3e955
Priyanka <priyankamohan2706@gmail.com>
2026-06-30 20:01:52 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+
+return (
+
+<>
+
+<Navbar/>
+
+<main>
+
+<Hero/>
+
+</main>
+
+</>
+
+);
+
+}
